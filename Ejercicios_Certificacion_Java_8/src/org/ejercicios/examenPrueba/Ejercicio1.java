5663e2de7a0ff6ab5eaff990b8ebab2f1fbc95b0
javabuk <joprieto@gmail.com>
2019-06-18 18:40:21 +0200

    Primer ejercicio del examen de prueba de whizlabs

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/examenPrueba/Ejercicio1.java
@@ -0,0 +1,29 @@
+package org.ejercicios.examenPrueba;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Path p1 = Paths.get("C:\\Users\\Jorge\\Git\\certificacion\\Ejercicios_Certificacion_Java_8\\src\\org\\ejercicios\\examenPrueba\\file1.txt");
+		Path p2 = Paths.get("C:\\Users\\Jorge\\Git\\certificacion\\Ejercicios_Certificacion_Java_8\\src\\org\\ejercicios\\examenPrueba\\file2.txt");
+		try {
+			Files.copy(p1, p2, StandardCopyOption.COPY_ATTRIBUTES);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
+/**
+ * OK
+ * Respuecta correcta: 
+ * B- An exception is thrown at runtime
+ */
