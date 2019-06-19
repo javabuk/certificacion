1cedd7dd455d5a4041c934c3e80bd0b448c8a5e2
javabuk <joprieto@gmail.com>
2019-06-19 19:13:50 +0200

    Ejercicio 14

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/examenPrueba/ejercicio14/MyApp.java
@@ -0,0 +1,43 @@
+package org.ejercicios.examenPrueba.ejercicio14;
+
+import java.io.IOException;
+
+public class MyApp implements AutoCloseable {
+
+	
+	public static void main(String[] args) {
+		try ( MyApp myApp = new MyApp()){
+			myApp.open();
+			myApp.read();
+			myApp.close();
+		}catch (IOException e){
+			System.out.println("Exception caught");
+		}
+
+	}
+
+	public String read() throws IOException {
+		System.out.println("I am an exception");
+		throw new IOException();
+	}
+
+	public void open() {
+		System.out.println("Opening MyApp ...");
+	}
+
+	@Override
+	public void close() {
+		System.out.println("Closing MyApp ...");
+	}
+}
+/**
+ * OK
+ * Respuesta:
+ * B- 
+ * Opening MyApp ...
+I am an exception
+Closing MyApp ...
+Exception caught
+ * 
+ * 
+ */
