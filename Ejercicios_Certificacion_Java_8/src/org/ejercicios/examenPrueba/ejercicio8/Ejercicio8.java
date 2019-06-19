e2d4d5db699685ad3dd62bc34b39441be0ff5ac0
javabuk <joprieto@gmail.com>
2019-06-19 18:26:07 +0200

    Ejercicio8

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/examenPrueba/ejercicio8/Ejercicio8.java
@@ -0,0 +1,27 @@
+package org.ejercicios.examenPrueba.ejercicio8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		
+		//List<Object> list = new ArrayList<Object>(Arrays.asList("a","b")); //A
+		//List<String> list = new ArrayList<Object>(Arrays.asList("a","b")); //B
+		//List<Object> list = new ArrayList<String>(Arrays.asList("a","b")); //C
+		List<Object> list = new ArrayList<>(Arrays.asList("a","b")); //D
+		//List<> list = new ArrayList<String>(Arrays.asList("a","b")); //E
+
+	}
+
+}
+/**
+ * OK
+ * Respuestas A,D
+ * 
+ * El resto de opciones dan errores de compilación.
+ * 
+ * 
+ */
