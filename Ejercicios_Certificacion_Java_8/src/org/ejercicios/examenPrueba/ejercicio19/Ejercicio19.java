e53ec1d110767d756ee3ee52a163b1c80c37cd3e
javabuk <joprieto@gmail.com>
2019-06-20 18:40:34 +0200

    Ejercicio 19

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/examenPrueba/ejercicio19/Ejercicio19.java
@@ -0,0 +1,21 @@
+package org.ejercicios.examenPrueba.ejercicio19;
+
+public class Ejercicio19 {
+
+	public static void main(String[] args) {
+		int number1 = 0b111;
+		int number2 = 0_111;
+		int number3 = 0x111;
+		
+		System.out.println(number1 + number2 + number3);
+
+	}
+
+}
+/**
+ * OK
+ * 
+ * Respuesta:
+ * E - 353
+ *  
+ */
