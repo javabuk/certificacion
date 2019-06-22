b62fc3278380ec6b21857a89ebb331c6bf13033a
javabuk <joprieto@gmail.com>
2019-06-22 08:55:49 +0200

    Ejercicio 5

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/libro/exceptions/ej5/Igloo.java
@@ -0,0 +1,14 @@
+package org.ejercicios.libro.exceptions.ej5;
+
+public class Igloo {
+	public static void main(String[] bricks) {
+	int flakes = 10;
+	double assert = 7.0;
+	assert (true :"");
+	assert flakes++>5;
+ }
+}
+/*
+*	Respuesta:
+*	D - Two lines of code does not compile
+*/
