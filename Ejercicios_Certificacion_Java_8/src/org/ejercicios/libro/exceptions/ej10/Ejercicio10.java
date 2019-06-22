c1281553fa71165f6d3e2a75f2e07bae24d1f077
javabuk <joprieto@gmail.com>
2019-06-22 12:38:21 +0200

    Ejercicio 10

---
+++ Ejercicios_Certificacion_Java_8/src/org/ejercicios/libro/exceptions/ej10/Ejercicio10.java
@@ -0,0 +1,15 @@
+package org.ejercicios.libro.exceptions.ej10;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		// assert(0,"Invalid"); A- Error de compilación
+		//assert 0==1;
+		assert 0==0;
+	}
+
+}
+/*
+ * Respuesta:
+ * D- None of the above
+ */
